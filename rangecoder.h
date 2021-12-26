f3658c0c2c723141cc8cd3e6f1db65b20eac00b4
Hiroki-Kojima <iamhirok@icloud.com>
2021-12-26 23:58:10 +0900

    reformat

--- rangecoder.h
+++ rangecoder.h
@@ -1,6 +1,7 @@
 #pragma once
 #ifndef RANGECODER_H_
 #define RANGECODER_H_
+
 #include <iomanip>
 #include <iostream>
 #include <limits>
@@ -14,157 +15,163 @@ namespace rangecoder
     using range_t = uint64_t;
     using byte_t = uint8_t;
 
-namespace local
-{
-    const auto TOP8 = range_t(1) << (64 - 8);
-    const auto TOP16 = range_t(1) << (64 - 16);
-
-    auto hex_zero_filled(range_t bytes) -> std::string
+    namespace local
     {
-        std::stringstream sformatter;
-        sformatter << std::setfill('0') << std::setw(sizeof(range_t) * 2) << std::hex << bytes;
-        return sformatter.str();
-    }
-    auto hex_zero_filled(byte_t byte) -> std::string
-    {
-        std::stringstream sformatter;
-        sformatter << std::setfill('0') << std::setw(2) << std::hex << static_cast<int>(byte);
-        return sformatter.str();
-    }
+        const auto TOP8 = range_t(1) << (64 - 8);
+        const auto TOP16 = range_t(1) << (64 - 16);
 
-    class RangeCoder
-    {
-    public:
-        RangeCoder()
+        auto hex_zero_filled(range_t bytes) -> std::string
         {
-            m_lower_bound = 0;
-            m_range = std::numeric_limits<range_t>::max();
-        };
-
-        auto update_param(range_t c_freq, range_t cum_freq, range_t total_freq) -> std::vector<byte_t>
-        {
-            auto bytes = std::vector<byte_t>();
-
-            auto range_per_total = m_range / total_freq;
-            m_range = range_per_total * c_freq;
-            m_lower_bound += range_per_total * cum_freq;
-
-#ifdef RANGECODER_VERBOSE
-            std::cout << "  range, lower bound updated" << std::endl;
-            print_status();
-#endif
-            while (is_no_carry_expansion_needed())
-            {
-                bytes.push_back(do_no_carry_expansion());
-            }
-            while (is_range_reduction_expansion_needed())
-            {
-                bytes.push_back(do_range_reduction_expansion());
-            }
-#ifdef RANGECODER_VERBOSE
-            std::cout << "  " << bytes.size() << " byte shifted" << std::endl;
-#endif
-            return bytes;
-        };
-
-        auto shift_byte() -> byte_t
-        {
-            auto tmp = static_cast<byte_t>(m_lower_bound >> (64 - 8));
-            m_range <<= 8;
-            m_lower_bound <<= 8;
-#ifdef RANGECODER_VERBOSE
-            std::cout << "  shifted out byte: "
-                      << "0x"
-                      << local::hex_zero_filled(tmp)
-                      << std::endl;
-#endif
-            return tmp;
-        };
-
-        void print_status() const
-        {
-            std::cout << "        range: "
-                      << "0x" << local::hex_zero_filled(range()) << std::endl;
-            std::cout << "  lower bound: "
-                      << "0x" << local::hex_zero_filled(lower_bound()) << std::endl;
+            std::stringstream sformatter;
+            sformatter << std::setfill('0') << std::setw(sizeof(range_t) * 2) << std::hex << bytes;
+            return sformatter.str();
         }
 
-    protected:
-        void lower_bound(const range_t lower_bound)
+        auto hex_zero_filled(byte_t byte) -> std::string
         {
-            m_lower_bound = lower_bound;
-        };
+            std::stringstream sformatter;
+            sformatter << std::setfill('0') << std::setw(2) << std::hex << static_cast<int>(byte);
+            return sformatter.str();
+        }
 
-        void range(const range_t range)
+        class RangeCoder
         {
-            m_range = range;
-        };
+        public:
+            RangeCoder()
+            {
+                m_lower_bound = 0;
+                m_range = std::numeric_limits<range_t>::max();
+            };
 
-        auto lower_bound() const -> range_t
-        {
-            return m_lower_bound;
-        };
+            auto update_param(range_t c_freq, range_t cum_freq, range_t total_freq) -> std::vector<byte_t>
+            {
+                auto bytes = std::vector<byte_t>();
 
-        auto range() const -> range_t
-        {
-            return m_range;
-        };
+                auto range_per_total = m_range / total_freq;
+                m_range = range_per_total * c_freq;
+                m_lower_bound += range_per_total * cum_freq;
 
-    private:
-        auto is_no_carry_expansion_needed() -> bool
-        {
-            return (m_lower_bound ^ upper_bound()) < local::TOP8;
-        };
-
-        auto do_no_carry_expansion() -> byte_t
-        {
 #ifdef RANGECODER_VERBOSE
-            std::cout << "  no carry expansion" << std::endl;
+                std::cout << "  range, lower bound updated" << std::endl;
+                print_status();
 #endif
-            return shift_byte();
-        };
-
-        auto is_range_reduction_expansion_needed() -> bool
-        {
-            return m_range < local::TOP16;
-        };
-
-        auto do_range_reduction_expansion() -> byte_t
-        {
+                while (is_no_carry_expansion_needed())
+                {
+                    bytes.push_back(do_no_carry_expansion());
+                }
+                while (is_range_reduction_expansion_needed())
+                {
+                    bytes.push_back(do_range_reduction_expansion());
+                }
 #ifdef RANGECODER_VERBOSE
-            std::cout << "  range reduction expansion" << std::endl;
+                std::cout << "  " << bytes.size() << " byte shifted" << std::endl;
 #endif
-            m_range = !(m_lower_bound & (local::TOP16 - 1));
-            return shift_byte();
-        };
+                return bytes;
+            };
 
-        auto upper_bound() const -> uint64_t
-        {
-            return m_lower_bound + m_range;
-        };
+            auto shift_byte() -> byte_t
+            {
+                auto tmp = static_cast<byte_t>(m_lower_bound >> (64 - 8));
+                m_range <<= 8;
+                m_lower_bound <<= 8;
+#ifdef RANGECODER_VERBOSE
+                std::cout << "  shifted out byte: "
+                          << "0x"
+                          << local::hex_zero_filled(tmp)
+                          << std::endl;
+#endif
+                return tmp;
+            };
 
-        uint64_t m_lower_bound;
-        uint64_t m_range;
-    };
-}// namespace rangecoder::local
+            void print_status() const
+            {
+                std::cout << "        range: "
+                          << "0x" << local::hex_zero_filled(range()) << std::endl;
+                std::cout << "  lower bound: "
+                          << "0x" << local::hex_zero_filled(lower_bound()) << std::endl;
+            }
+
+        protected:
+            void lower_bound(const range_t lower_bound)
+            {
+                m_lower_bound = lower_bound;
+            };
+
+            void range(const range_t range)
+            {
+                m_range = range;
+            };
+
+            auto lower_bound() const -> range_t
+            {
+                return m_lower_bound;
+            };
+
+            auto range() const -> range_t
+            {
+                return m_range;
+            };
+
+        private:
+            auto is_no_carry_expansion_needed() -> bool
+            {
+                return (m_lower_bound ^ upper_bound()) < local::TOP8;
+            };
+
+            auto do_no_carry_expansion() -> byte_t
+            {
+#ifdef RANGECODER_VERBOSE
+                std::cout << "  no carry expansion" << std::endl;
+#endif
+                return shift_byte();
+            };
+
+            auto is_range_reduction_expansion_needed() -> bool
+            {
+                return m_range < local::TOP16;
+            };
+
+            auto do_range_reduction_expansion() -> byte_t
+            {
+#ifdef RANGECODER_VERBOSE
+                std::cout << "  range reduction expansion" << std::endl;
+#endif
+                m_range = !(m_lower_bound & (local::TOP16 - 1));
+                return shift_byte();
+            };
+
+            auto upper_bound() const -> uint64_t
+            {
+                return m_lower_bound + m_range;
+            };
+
+            uint64_t m_lower_bound;
+            uint64_t m_range;
+        };
+    }// namespace local
 
     class PModel
     {
     public:
         // Accumulated frequency of index, i.e. sum of frequency of range [min_index, index).
         virtual range_t cum_freq(int index) const = 0;
+
         // Frequency of index
         virtual range_t c_freq(int index) const = 0;
+
         range_t total_freq() const
         {
             return cum_freq(max_index()) + c_freq(max_index());
         };
+
         // Returns min index, the first valid index.
         // All index 'i', that satisfy 'pmodel.min_index() <= i <= pmodel.max_index()' must be valid index.
         virtual int min_index() const = 0;
+
         // Returns max index, the last valid index.
         // All index 'i', that satisfy 'pmodel.min_index() <= i <= pmodel.max_index()' must be valid index.
         virtual int max_index() const = 0;
+
         bool index_is_valid(int index)
         {
             return min_index() <= index && index <= max_index();
@@ -304,22 +311,27 @@ namespace local
         constexpr UniformDistribution()
         {
         }
+
         range_t c_freq(int index) const
         {
             return 1;
         }
+
         range_t cum_freq(int index) const
         {
             return index;
         }
+
         int min_index() const
         {
             return 0;
         }
+
         int max_index() const
         {
             return N - 1;
         }
+
         void print() const
         {
             std::cout << std::endl;
