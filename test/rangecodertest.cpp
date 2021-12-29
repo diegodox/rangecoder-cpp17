f8b9dfd353ab26afbcd65fea569e1b1c4399afb6
diegodox <37207872+diegodox@users.noreply.github.com>
2021-12-30 02:02:48 +0900

    Add uniform large precision test (#23)
    * add large uniform distribution test
    
    * add 2,4,16 level uniform distribution test

--- test/rangecodertest.cpp
+++ test/rangecodertest.cpp
@@ -201,6 +201,148 @@ auto test_uniform_big(const std::vector<int> &data) -> std::vector<int>
     return decoded;
 }
 
+auto test_uniform_binary(const std::vector<bool> &data) -> std::vector<bool>
+{
+    // pmodel
+    std::cout << "create pmodel" << std::endl;
+    const auto pmodel = rangecoder::UniformDistribution<2>();
+    // encode
+    std::cout << "encode" << std::endl;
+    auto enc = rangecoder::RangeEncoder();
+    for (int i = 0; i < data.size(); i++)
+    {
+        std::cout << std::dec << i << "  encode: " << data[i] << std::endl;
+        enc.print_status();
+        enc.encode(pmodel, data[i]);
+    }
+    enc.print_status();
+    const auto bytes = enc.finish();
+
+    std::cout << "encoded bytes: "
+              << "0x" << rangecoder::local::hex_zero_filled(bytes[0]);
+    for (auto byte : bytes)
+    {
+        std::cout << rangecoder::local::hex_zero_filled(byte);
+    }
+    std::cout << std::endl;
+
+    // decode
+    auto que = std::queue<rangecoder::byte_t>();
+    for (auto byte : bytes)
+    {
+        que.push(byte);
+    }
+    std::cout << "decode" << std::endl;
+    auto dec = rangecoder::RangeDecoder();
+    dec.start(que);
+    auto decoded = std::vector<bool>();
+    for (int i = 0; i < data.size(); i++)
+    {
+        dec.print_status();
+        auto d = dec.decode(pmodel);
+        std::cout << std::dec << i << "  decode: " << d << std::endl;
+        decoded.push_back(d);
+    }
+    dec.print_status();
+    std::cout << "finish" << std::endl;
+    return decoded;
+}
+
+auto test_uniform_4(const std::vector<int> &data) -> std::vector<int>
+{
+    // pmodel
+    std::cout << "create pmodel" << std::endl;
+    const auto pmodel = rangecoder::UniformDistribution<4>();
+    // encode
+    std::cout << "encode" << std::endl;
+    auto enc = rangecoder::RangeEncoder();
+    for (int i = 0; i < data.size(); i++)
+    {
+        std::cout << std::dec << i << "  encode: " << data[i] << std::endl;
+        enc.print_status();
+        enc.encode(pmodel, data[i]);
+    }
+    enc.print_status();
+    const auto bytes = enc.finish();
+
+    std::cout << "encoded bytes: "
+              << "0x" << rangecoder::local::hex_zero_filled(bytes[0]);
+    for (auto byte : bytes)
+    {
+        std::cout << rangecoder::local::hex_zero_filled(byte);
+    }
+    std::cout << std::endl;
+
+    // decode
+    auto que = std::queue<rangecoder::byte_t>();
+    for (auto byte : bytes)
+    {
+        que.push(byte);
+    }
+    std::cout << "decode" << std::endl;
+    auto dec = rangecoder::RangeDecoder();
+    dec.start(que);
+    auto decoded = std::vector<int>();
+    for (int i = 0; i < data.size(); i++)
+    {
+        dec.print_status();
+        auto d = dec.decode(pmodel);
+        std::cout << std::dec << i << "  decode: " << d << std::endl;
+        decoded.push_back(d);
+    }
+    dec.print_status();
+    std::cout << "finish" << std::endl;
+    return decoded;
+}
+
+auto test_uniform_16(const std::vector<int> &data) -> std::vector<int>
+{
+    // pmodel
+    std::cout << "create pmodel" << std::endl;
+    const auto pmodel = rangecoder::UniformDistribution<16>();
+    // encode
+    std::cout << "encode" << std::endl;
+    auto enc = rangecoder::RangeEncoder();
+    for (int i = 0; i < data.size(); i++)
+    {
+        std::cout << std::dec << i << "  encode: " << data[i] << std::endl;
+        enc.print_status();
+        enc.encode(pmodel, data[i]);
+    }
+    enc.print_status();
+    const auto bytes = enc.finish();
+
+    std::cout << "encoded bytes: "
+              << "0x" << rangecoder::local::hex_zero_filled(bytes[0]);
+    for (auto byte : bytes)
+    {
+        std::cout << rangecoder::local::hex_zero_filled(byte);
+    }
+    std::cout << std::endl;
+
+    // decode
+    auto que = std::queue<rangecoder::byte_t>();
+    for (auto byte : bytes)
+    {
+        que.push(byte);
+    }
+    std::cout << "decode" << std::endl;
+    auto dec = rangecoder::RangeDecoder();
+    dec.start(que);
+    auto decoded = std::vector<int>();
+    for (int i = 0; i < data.size(); i++)
+    {
+        dec.print_status();
+        auto d = dec.decode(pmodel);
+        std::cout << std::dec << i << "  decode: " << d << std::endl;
+        decoded.push_back(d);
+    }
+    dec.print_status();
+    std::cout << "finish" << std::endl;
+    return decoded;
+}
+
+// test rangecoder with frequency table.
 TEST(RangeCoderTest, EncDecTest)
 {
     const auto data = std::vector<int>{1, 2, 3, 4, 5, 8, 3, 2, 1, 0, 3, 7};
@@ -208,6 +350,7 @@ TEST(RangeCoderTest, EncDecTest)
     std::cout << "finish" << std::endl;
 }
 
+// test rangecoder with 256 level (8bit) uniform distribution.
 TEST(RangeCoderTest, UniformDistributionTest)
 {
     const auto data = std::vector<int>{1, 2, 3, 4, 5, 8, 3, 2, 1, 0, 3, 7};
@@ -215,6 +358,31 @@ TEST(RangeCoderTest, UniformDistributionTest)
     std::cout << "finish" << std::endl;
 }
 
+// test rangecoder with 2 level (1bit) uniform distribution.
+TEST(RangeCoderTest, UniformBinaryDistributionTest)
+{
+    const auto data = std::vector<bool>{true, false, true, true, false, true, false, false, true, true, true, true};
+    EXPECT_EQ(test_uniform_binary(data), data);
+    std::cout << "finish" << std::endl;
+}
+
+// test rangecoder with 4 level uniform distribution.
+TEST(RangeCoderTest, UniformDistributionTest4)
+{
+    const auto data = std::vector<int>{1, 2, 3, 2, 3, 2, 3, 2, 1, 0, 3, 1};
+    EXPECT_EQ(test_uniform_4(data), data);
+    std::cout << "finish" << std::endl;
+}
+
+// test rangecoder with 16 level (4bit) uniform distribution.
+TEST(RangeCoderTest, UniformDistributionTest16)
+{
+    const auto data = std::vector<int>{1, 5, 3, 15, 2, 7, 9, 2, 1, 0, 3, 1};
+    EXPECT_EQ(test_uniform_16(data), data);
+    std::cout << "finish" << std::endl;
+}
+
+// test rangecoder with 65536 level (16bit) uniform distribution.
 TEST(RangeCoderTest, UniformDistributionBigTest)
 {
     const auto data = std::vector<int>{1, 2, 3, 4, 5, 65533, 3, 2, 1, 0, 3, 7};
